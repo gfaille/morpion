41e221fddaac36d2f8032654fd37a171552bb513
gfaille <zerteducon@gmail.com>
2022-09-22 14:44:36 +0200

    ajout de la vérification des coups jouer.

--- morpion.py
+++ morpion.py
@@ -10,23 +10,25 @@ def afficher_grille (grille) :
         print (i)
 
 def jouer_coups () :
-    """fonction pour demander au joueur de jouer un coups entre 1 à 9."""
+    """fonction pour demander au joueur de jouer un coups entre 1 à 9 et vérifier si le coups est valide."""
 
     afficher_grille(liste_case)
     print("tour du joueur. "  + "tapez de 1 à 9")
     coups_jouer = int(input())
     if coups_jouer >= 7:
-        #if coups_jouer == "_" :
-
-        liste_case[0][coups_jouer-7] = "X"
-        afficher_grille(liste_case)
+        if liste_case[0][coups_jouer-7] == "_" :
+            liste_case[0][coups_jouer-7] = "X"
+            afficher_grille(liste_case)
     elif coups_jouer >= 4:
-        liste_case[1][coups_jouer-4] = "C"
-        afficher_grille(liste_case)
+        if liste_case[1][coups_jouer-4] == "_" :
+            liste_case[1][coups_jouer-4] = "C"
+            afficher_grille(liste_case)
     elif coups_jouer >= 1:
-        liste_case[2][coups_jouer-1] = "V"
-        afficher_grille(liste_case)
+        if liste_case[1][coups_jouer-4] == "_" :
+            liste_case[2][coups_jouer-1] = "V"
+            afficher_grille(liste_case)
     else:
         print("erreur 2")
+
 
 jouer_coups()
