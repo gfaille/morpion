0503badd6a04824105a505f1516eeb14b3e3f003
gfaille <zerteducon@gmail.com>
2022-09-22 14:14:08 +0200

    enlever les fonction non fonctionnel, crée nouvelle fonction pour jouer un coups

--- morpion.py
+++ morpion.py
@@ -4,21 +4,29 @@ liste_case = [
     ["_", "_", "_"]
 ]
 
-def afficher_grille () :
+def afficher_grille (grille) :
     """ fonction pour créer la grille du morpion sur une ligne de 3X3. """
-    for i in range (3) :
-        print (liste_case[i])
+    for i in grille :
+        print (i)
 
 def jouer_coups () :
-    """fonction pour jouer un coups soit "X" soit "O" dans la grille"""
+    """fonction pour demander au joueur de jouer un coups entre 1 à 9."""
 
-    print("tour du joueur. " + joueur + " tapez de 1 à 9")
-    placer = int(input()) - 1
+    afficher_grille(liste_case)
+    print("tour du joueur. "  + "tapez de 1 à 9")
+    coups_jouer = int(input())
+    if coups_jouer >= 7:
+        #if coups_jouer == "_" :
 
-    if liste_case[placer] == "_" :
-        placer = joueur 
-    else :
-        print("case occuper veuillez recommencez ailleur")
-        continue 
-    
+        liste_case[0][coups_jouer-7] = "X"
+        afficher_grille(liste_case)
+    elif coups_jouer >= 4:
+        liste_case[1][coups_jouer-4] = "C"
+        afficher_grille(liste_case)
+    elif coups_jouer >= 1:
+        liste_case[2][coups_jouer-1] = "V"
+        afficher_grille(liste_case)
+    else:
+        print("erreur 2")
+
 jouer_coups()
