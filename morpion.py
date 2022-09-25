6e1c1bc087839fae0c9fefe220a4ce99bb74ef55
gfaille <zerteducon@gmail.com>
2022-09-25 14:29:53 +0200

    correction du plantage lors d'entrer d'un nombre supperieur à 9 par l'utilisateur

--- morpion.py
+++ morpion.py
@@ -8,6 +8,8 @@ def afficher_grille (grille) :
     """ fonction pour créer la grille du morpion sur une ligne de 3X3. """
     for i in grille :
         print (i)
+
+
 def jouer_coups () :
     """fonction pour demander au joueur de jouer un coups entre 1 à 9 et vérifier si le coups est valide."""
     while True :
@@ -15,26 +17,29 @@ def jouer_coups () :
         afficher_grille(liste_case)
         print("tour du joueur. "  + joueur + " tapez de 1 à 9")
         coups_jouer = int(input())
-        if coups_jouer >= 7 :
-            if liste_case[0][coups_jouer-7] == "_" :
-                liste_case[0][coups_jouer-7] = joueur
-                return
-            else :
-                print("case déjâ utiliser !")
-        elif coups_jouer >= 4:
-            if liste_case[1][coups_jouer-4] == "_" :
-                liste_case[1][coups_jouer-4] = joueur
-                return
-            else :
-                print("case déjâ utiliser !")
-        elif coups_jouer >= 1:
-            if liste_case[2][coups_jouer-1] == "_" :
-                liste_case[2][coups_jouer-1] = joueur
-                return
-            else :
-                print("case déjâ utiliser !")
-        else:
-            print("erreur 2")
+        if coups_jouer < 10 :
+            if coups_jouer >= 7 :
+                if liste_case[0][coups_jouer-7] == "_" :
+                    liste_case[0][coups_jouer-7] = joueur
+                    return
+                else :
+                    print("case déjâ utiliser !")
+            elif coups_jouer >= 4:
+                if liste_case[1][coups_jouer-4] == "_" :
+                    liste_case[1][coups_jouer-4] = joueur
+                    return
+                else :
+                    print("case déjâ utiliser !")
+            elif coups_jouer >= 1:
+                if liste_case[2][coups_jouer-1] == "_" :
+                    liste_case[2][coups_jouer-1] = joueur
+                    return
+                else :
+                    print("case déjâ utiliser !")
+            else:
+                print("erreur 24")
+        else :
+            return print("veuillez jouer un coups valide ! ")
 
 joueur = "X"
 tour = 0
@@ -73,7 +78,7 @@ while True :
             joueur = "joueur 2"
         print(joueur + " Gagné")
         break
-
+    
     jouer_coups()
     if tour == 9 :
         print ("égality")
